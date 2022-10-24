29c0230035e0845347ce302d2f08ba6adc4d8ffa
Robert A <robert@roadshift.io>
2022-10-23 20:23:49 -0700

    rust basic data types

---
+++ data_types/src/main.rs
@@ -0,0 +1,21 @@
+
+// Basic Data Types in Rust
+fn main() {    
+    // boolean 
+    let a = true
+    let b = false 
+
+    // integer
+    let c = 2 
+    let d = -3
+
+    // double / float
+    let e = 1.1
+    let f = 200.029319
+
+    // char
+    let g = 'A' 
+
+    // string 
+    let f = "Hello"
+}
